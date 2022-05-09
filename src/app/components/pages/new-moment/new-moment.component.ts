f9e4f8c120d870c4dd91df38c95fdcae7ed895e7
Celio Junior <celio_cjr@hotmail.com>
2022-05-09 07:40:52 -0300

    criando o componente de mensagem e exibicao dinamica

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moments';
+import { MomentsService } from 'src/app/services/moments.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -8,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 export class NewMomentComponent implements OnInit {
   btnText = "Share";
 
-  constructor() { }
+  constructor(private momentsService: MomentsService) { }
 
   ngOnInit(): void {
+  }
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append("title", moment.title);
+    formData.append("description", moment.description);
+
+    if(moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    // enviar para o service
+    await this.momentsService.createMoment(formData).subscribe();
+    // exibir messagem
+
+    // redirect para outra pagina
   }
 
 }
