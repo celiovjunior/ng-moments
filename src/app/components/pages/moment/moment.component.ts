dc1cbcd3c842e70c4e3eb69492b95ab8cc60cf07
Celio Junior <celio_cjr@hotmail.com>
2022-05-11 23:52:41 -0300

    criando pagina de exibicao de detalhes de um unico momento e implementando funcionalidades de edicao

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MomentsService } from 'src/app/services/moments.service';
 import { Moment } from 'src/app/Moments';
+import { environment } from 'src/environments/environment';
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -11,6 +13,9 @@ import { Moment } from 'src/app/Moments';
 export class MomentComponent implements OnInit {
 
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(private momentsService: MomentsService, private route: ActivatedRoute) { }
 
