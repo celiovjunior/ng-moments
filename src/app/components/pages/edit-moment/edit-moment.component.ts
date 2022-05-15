2bb0ee79b7852e444a061628fab14cd09bea2421
Celio Junior <celio_cjr@hotmail.com>
2022-05-15 00:08:50 -0300

    finalizando a funcionalidade de editar um momento, transferindo o formulario e fazendo as modificacoes

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Moment } from 'src/app/Moments';
+import { MomentsService } from 'src/app/services/moments.service';
 
 @Component({
   selector: 'app-edit-moment',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-moment.component.css']
 })
 export class EditMomentComponent implements OnInit {
+  moment!: Moment;
+  btnText: string = "Edit";
 
-  constructor() { }
+  constructor(private momentsService: MomentsService, private route: ActivatedRoute){ }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+
+    this.momentsService.getMoment(id).subscribe(item => {
+      this.moment = item.data;
+    })
   }
 
 }
